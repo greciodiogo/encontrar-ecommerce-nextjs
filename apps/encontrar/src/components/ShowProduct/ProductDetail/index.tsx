cfe4e25f3071252f77f402d21a2abf86035c1535
Grécio Diogo <fonebahia8@gmail.com>
2025-02-09 20:56:05 +0100

    fix: handle click on produtct detail page

--- apps/encontrar/src/components/ShowProduct/ProductDetail/index.tsx
+++ apps/encontrar/src/components/ShowProduct/ProductDetail/index.tsx
@@ -1,21 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useRouter } from 'next/router';
 import React from 'react';
 
+import { addToCart } from 'actions/products';
 import { adjustQty } from 'actions/products';
 import { ChangeQuantity } from 'shared/ChangeQuantity';
 import { SubmitButton } from 'shared/SubmitButton';
 import { ProductDetailProps } from 'types/product';
 
-import { useAppDispatch } from '../../../hooks';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
 
 export const ProductDetail = (props: ProductDetailProps) => {
   const url = 'assets_ecommerce';
   const { name, availability, category, price, brand, id = 0, qty } = props.product;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const product = useAppSelector((state: any) => state.products.currentItem);
+  const router = useRouter();
   const dispatch = useAppDispatch();
+
+  const handleCheckoutBtnClick = () => {
+    void router.push('/cart');
+  };
+
+  const handleAddToCart = (id: number) => {
+    dispatch(addToCart(id));
+    // setShowCheckout(true);() => handleAddToCart(product.id)
+  };
 
   const handleAdjustQtyCart = (id: number, value: number) => {
     if (value < 1) return; // Evita valores negativos ou zero
     dispatch(adjustQty(id, value));
   };
+
   return (
     <div className="wrapper">
       <div className="star_container">
@@ -48,8 +68,8 @@ export const ProductDetail = (props: ProductDetailProps) => {
       <div className="cart-item-btn">
         <div className="change_quantity">
           <ChangeQuantity id={id} qty={qty ?? 0} onAdjustQty={handleAdjustQtyCart} />
-          <SubmitButton title="Adicionar ao Carrinho" svg="cart-2" />
-          <SubmitButton title="Comprar agora" outlined={true} />
+          <SubmitButton onClick={() => handleAddToCart(product.id)} title="Adicionar ao Carrinho" svg="cart-2" />
+          <SubmitButton onClick={handleCheckoutBtnClick} title="Comprar agora" outlined={true} />
         </div>
       </div>
     </div>
