b9caf123e69581b04ebc4449d1006d4f2e479c16
Grécio Diogo <fonebahia8@gmail.com>
2025-02-10 13:14:02 +0100

    feat: add react slick on banner component

--- apps/encontrar/src/components/Banner/index.tsx
+++ apps/encontrar/src/components/Banner/index.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+'use client';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const information = [
+  'Tenha Acesso aos Melhores Aparelhos da Banda',
+  'Tenha Acesso aos Melhores Aparelhos da Banda',
+  'Tenha Acesso aos Melhores Aparelhos da Banda',
+  'Tenha Acesso aos Melhores Aparelhos da Banda',
+];
 
 export const Banner = () => {
-  const information = [
-    'Tenha Acesso aos Melhores Aparelhos da Banda',
-    'Tenha Acesso aos Melhores Aparelhos da Banda',
-    'Tenha Acesso aos Melhores Aparelhos da Banda',
-    'Tenha Acesso aos Melhores Aparelhos da Banda',
-  ];
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1024, // Para telas menores que 1024px
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768, // Para telas menores que 768px
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true, // Centraliza o slide
+          centerPadding: '0px', // Remove margem latera
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="banner">
-      {information.map((info, index) => (
-        <p key={index} className="banner_text">
-          {info}
-        </p>
-      ))}
+    <div className="slider-container">
+      <Slider {...settings}>
+        {information.map((text, index) => (
+          <div className="slide-item" key={index}>
+            <h3 className="banner_text">{text}</h3>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
