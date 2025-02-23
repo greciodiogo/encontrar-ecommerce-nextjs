6c30f72f6b7d13e25c6ccec04b5891dd3fcca1e5
Grécio Diogo <fonebahia8@gmail.com>
2025-02-24 00:06:28 +0100

    chore: adjust next.config.js file

--- apps/encontrar/next.config.js
+++ apps/encontrar/next.config.js
@@ -2,23 +2,23 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextTranslate = require('next-translate-plugin');
 
-// const nextConfig = {
-//   swcMinify: true,
-//   reactStrictMode: true,
-//   modularizeImports: {
-//     '@mui/icons-material': {
-//       transform: '@mui/icons-material/{{member}}',
-//     },
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-// };
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-// module.exports = nextTranslate(nextConfig);
+module.exports = nextTranslate(nextConfig);
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 module.exports = nextTranslate();
