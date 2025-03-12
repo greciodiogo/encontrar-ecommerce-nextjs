a21d3df7941cd3ddc0ed99496bd28e34fbb3813c
Grécio Diogo <fonebahia8@gmail.com>
2025-03-12 23:53:17 +0100

    fix: add send email endppint

--- apps/encontrar/src/pages/api/send-email.ts
+++ apps/encontrar/src/pages/api/send-email.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 
@@ -7,24 +8,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for port 465, false for other ports
       auth: {
-        user: 'seu-email@gmail.com',
-        pass: 'sua-senha',
+        user: 'fonebahia8@gmail.com',
+        pass: 'D201702@@',
       },
     });
 
     const mailOptions = {
-      from: 'seu-email@gmail.com',
-      to: 'seu-email@gmail.com',
+      from: email,
+      to: 'fonebahia8@gmail.com',
       subject: subject,
       text: `Email de: ${email}\n\nMensagem: ${message}`,
+      html: '<b>Hello world?</b>', // html body
     };
 
     try {
       await transporter.sendMail(mailOptions);
       res.status(200).json({ message: 'Email enviado com sucesso' });
     } catch (error) {
-      res.status(500).json({ message: 'Erro ao enviar email' });
+      res.status(500).json({ message: 'Erro ao enviar email', error: error });
     }
   } else {
     res.status(405).json({ message: 'Método não permitido' });
