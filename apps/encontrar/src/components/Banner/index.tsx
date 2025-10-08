6c96418047c76cd1ed70677187cd139a9e481199
Grécio Diogo <fonebahia8@gmail.com>
2025-10-08 12:53:50 +0100

    fix: adjust component meta tags

--- apps/encontrar/src/components/Banner/index.tsx
+++ apps/encontrar/src/components/Banner/index.tsx
@@ -5,6 +5,7 @@ import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Head from 'next/head';
 
 export const Banner = () => {
   const { t } = useTranslation('home'); // 'common' se refere ao nome do JSON
@@ -45,8 +46,28 @@ export const Banner = () => {
     ],
   };
 
+  const meta = {
+    title: 'Encontrar – Encontre os melhores produtos aqui.',
+    description: `Tenha Acesso aos Melhores Aparelhos da Banda, Encontre os melhores produtos aqui.`,
+    image: 'https://encontrarshopping.com/logo.ico',
+    type: 'website',
+  };
+
   return (
     <div className="slider-container">
+      <Head>
+        <title>Encontrar</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`https://encontrarshopping.com${router.asPath}`} />
+        <link rel="canonical" href={`https://encontrarshopping.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Encontrar" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" name="robots" />
+      </Head>
       <Slider {...settings}>
         {information.map((text, index) => (
           <div className="slide-item" key={index}>
